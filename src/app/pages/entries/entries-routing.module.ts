73ce4b11a38f2325fb323587d1407e4c44461edb
rafael-queiroz <rafael.qp@hotmail.com>
2019-02-04 19:49:38 -0200

    adding EntryFormComponent

--- src/app/pages/entries/entries-routing.module.ts
+++ src/app/pages/entries/entries-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 const routes: Routes = [
   { path: '', component: EntryListComponent },  // master
-  //{ path: 'new', component: EntryFormComponent }, // detail
-  //{ path: ':id/edit', component: EntryFormComponent }, // detail
+  { path: 'new', component: EntryFormComponent }, // detail
+  { path: ':id/edit', component: EntryFormComponent }, // detail
 ];
 
 @NgModule({
