f0f4e6148018ed5db944117eee0cd6c7608e158d
rafael-queiroz <rafael.qp@hotmail.com>
2019-02-08 18:31:58 -0200

    adding ServerErrorMessagesComponent to Forms

--- src/app/shared/components/form-field-error/form-field-error.component.ts
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -34,7 +34,7 @@ export class FormFieldErrorComponent implements OnInit {
 
   private getErrorMessage(): string | null {
     if (this.formControl.errors.required)
-      return 'dado obigatório';
+      return 'dado obrigatório';
     
     else if (this.formControl.errors.email)
       return 'formato de email inválido';
